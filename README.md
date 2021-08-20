a6f88928dd889f66590f1978b254b513cb7b467a
Julie Rotter <mjrotter@msn.com>
2021-08-19 20:25:32 -0600

    final updates

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
 # UFOs
+*A JavaScript Interactive web-page*
+<p align="center">
+   <img width="1000" height="100" src="https://github.com/mjrotter4445/read_me_working_file/blob/main/Images/ufooverhead.png">
+</p>   
+
+## Project Overview
+### Purpose  
+The purpose of this project is to build a dynamic web-page that displays UFOs sightings observations information 
+for upcoming annual gathering of UFO enthusiasts in McMinnville, Oregon.  There is a lot of data to display so adding 
+filters to the table which lets uses refine their search on one of more than one level is abosolutely necessary.   
+
+### Background & the Process of Development
+In this development, we used **JavaScript** as the primary coding language.  JavaScript is a scripting or
+programming language that allows to implement complex features on web pages.  JavaScript is a front end development 
+used often to build extra functionality and customization into web pages.  Also used is the following list of resources
+and software.   
+
+### Resources
+The data for the table is stored in a JavaScript array or list named **data.js**
+The table is built by inserting **JavaScript** into **HTML** page. *app.py*.
+**HTML** is used to to build the webpage *index.html*. 
+**CSS** and **Boostrap** then builds and styles the page *style.css*.  
+Fire fox Developer Tools are ideal to test the code as we progress through the development.    
+Further we use a new Dependency named **D3**. 
+
+ ### Results
+Raw data from JavaScript array is now displayed in a dynamic table where userrs can filter data
+on multiple criteria by **date**, **city**, **country**, and **shape** of the UFO sighting.  Also, 
+including, with styling, the user is given an example of how to format their data entry with forward
+slashes, etc.. City, state, and country should be entered in lower case.  
+
+<p align="center">
+   <img width="1000" height="100" src="https://github.com/mjrotter4445/read_me_working_file/blob/main/Images/Heading_truth.png">
+</p>   
+
+<p align="center">
+   <img width="200" height="250" src="https://github.com/mjrotter4445/read_me_working_file/blob/main/Images/dynamic_multifilter.png">
+</p>   
+<p align="center">
+Dynamic multi-filter for user input
+</p>
+
+The user types their criteria in multi-filter and then press the enter the table displays
+only the rows that match the user input.   From the picutre below we can see the desired 
+data.  There is an important tip in using these search fields.  
+**The data must be entered in m/dd/yyy format**.  
+*Please follow the example above closely*.    
+
+<p align="center">
+   <img width="600" height="300" src="https://github.com/mjrotter4445/read_me_working_file/blob/main/Images/results.png">
+</p>   
+<p align="center">
+New table that displays only the results that matches the user input
+</p>
+
+When resetting the filter, the user has 2 choices.  user can either clear
+the input manually or deleting input cell by cell or click the refresh button
+*UFO Sightings* that can be found at the top left corner of the page.  After 
+the filter is cleared, the user can use filter again.    
+
+
+<p align="center">
+   <img width="200" height="175" src="https://github.com/mjrotter4445/read_me_working_file/blob/main/Images/howtoreset.png">
+</p>   
+<p align="center">
+Reset filter and refresh page button at the TOP LEFT CORNER of the web-page
+</p>
+
+### Summary
+### Drawbacks and Opportunities
+To build dynamic webpages that accept user inputs and visually adjust to reflect that
+interaction require quite a bit of work and patience.   I like the look of this page, 
+but it has some limitations in features that could definately be improved:  
+ - The **date entry format** could be improved to something more standard like mm/dd/yyyy. 
+ This would improve the user experience to something they are more used to using in daily life.  
+ - The **reset button location** could be designed to be closer to the filter input box.  It needs to be easier to see 
+ and access without scrolling to the top.
+      - Exploring the use of and investing in the time to develop **drop-down menus** would be worth it because it will 
+      signifcantly improve the user experience.  
+ - The **text case** is awkward to use and could be improved by coverting all letters to lower case from user input.  For example, 
+ Utah could be found if "ut" or "UT" were entered by the user.   
+ 
+ 
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,87 +1,85 @@
-// from data.js - all provided 
+// import data from data.js
 const tableData = data;
 
-// get table references - from 11.5.2
-const tbody = d3.select("tbody");
-///////////////////////////////////////////////////////////////////////////buildTable function
+// reference the HTML table(output) using d3 library
+var tbody = d3.select('tbody');
+
+// Function of populate data into html table
 function buildTable(data) {
-  // First, clear out any existing data
-  tbody.html("");
+    // init table data
+    tbody.html('');
 
-  // Next, loop through each object in the data
-  // and append a row and cells for each value in the row
-  data.forEach((dataRow) => {
-    // Append a row to the table body
-    const row = tbody.append("tr");
-
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
-    Object.values(dataRow).forEach((val) => {
-      const cell = row.append("td");
-      cell.text(val);
+    // first array loop for <tr>
+    data.forEach((dataRow) => {
+        let row = tbody.append('tr'); //html
+        //second loop for <td>
+        Object.values(dataRow).forEach((val) =>{
+            let cell = row.append('td'); //html
+            // d3 funtion 
+            cell.text(val);
+        });
     });
-  });
-}
-///////////////////////////////////////////////////////////////////////////var filters 
-// 1. Create a variable to keep track of all the filters as an object - my entry
-const filters = {};
 
-///////////////////////////////////////////////////////////////////////////updateFilters function
-// 3. Use this function to update the filters. Use let because used in this code block - in hint
+};
 
+// EXTRA: Create a variable to keep track of all the filters as an object.
+var clearEntries = d3.select("#clear-btn");
+clearEntries.on("click", function() {
+  location.reload();
+});
+
+
+
+// 1. Create a variable to keep track of all the filters as an object.
+var filters = {
+};
+
+// 3. Use this function to update the filters. 
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-    const changedElement = d3.select(this);
-    
+    let inputElement = d3.select(this);
+
     // 4b. Save the value that was changed as a variable.
-    const elementValue = changedElement.property("value");
-    //console.log(elementValue);
-    
+    let inputValue = inputElement.property("value");
+
     // 4c. Save the id of the filter that was changed as a variable.
-    const filterId = changedElement.attr("id");
-    //console.log(filterId);
+    let inputID = inputElement.attr("id");
   
     // 5. If a filter value was entered then add that filterId and value
-    // to the filters list. Otherwise, clear that filter from the filters object-from Jacob and Justin worktime
-     if(elementValue.trim()) {
-        filters[filterId] = elementValue;
-    }
-    else {
-    delete filters[filterId];
-    }
-  
-    // 6. Call function to apply all filters and rebuild the table - provided 
-    filterTable();
-  
-  }
+    // to the filters list. Otherwise, clear that filter from the filters object.
 
-///////////////////////////////////////////////////////////////////////////////filterTable function
-  // 7. Use this function to filter, called filterTable(), the table when data is entered-provided
-  function filterTable() {
+      if (inputValue) {
+        filters[inputID] = inputValue;
+    } else{filters ={};};
   
-    // 8. Set the filteredTable data to the tableData -  my entry, pretty sure. confirmed
-    var filteredTable = tableData;
   
-    // 9. Loop through all of the filters and keep any data that matches the filter values
-    //NEED HELP HERE pretty please 
+    // 6. Call function to apply all filters and rebuild the table
+    filterTable(filters);
+};
 
-    let filterDataOnly = Object.entries(filters);
-    console.log(filters);
-    console.log(filterDataOnly);
-    for (let [idKey, elValue] of filterDataOnly) {
-      filteredTable = filteredTable.filter(row => row[idKey] === elValue)
-      //console.log(idKey, elValue)
-    }
-
-    // 10. Finally, rebuild the table using the filtered data - 90% sure 
-      buildTable(filteredTable);  
-  }
+// 7. Use this function to filter the table when data is entered.
+function filterTable(obj) {
   
-  // 2.Attach an event to listen for changes to each filter-update all the data in UpdateFilters above-maybe right 80%
-  //d3.select("#filter-button").on("click", updateFilters);
+    // 8. Set the filtered data to the tableData.
+    let filteredData = tableData;
+  
+    // 9. Use a forEach Loop through all of the filters and keep any data that
+    // matches the filter values - work with Justin on this
+    
+    Object.entries(obj).forEach(([fkey, fval]) =>{
+        
+      filteredData = filteredData.filter((row) => row[fkey] === fval)
+          
 
-  d3.selectAll("input").on("change", updateFilters);
-
-  // Build the table when the page loads - provided 
+  });
+  
+    // 10. Finally, rebuild the table using the filtered data
+    buildTable(filteredData);
+};
+  
+  // 2. Attach an event to listen for changes to each filter
+  d3.selectAll("input").on("change",updateFilters);
+  
+  // Build the table when the page loads
   buildTable(tableData);
